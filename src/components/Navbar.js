880c227e8a8426ede092c88e621c6e8eb8c7ffa1
Hrishabh Jain <hrishabh507@gmail.com>
2023-04-16 19:17:50 +0530

    Minor UI changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,14 @@ const Navbar = () => {
   return (
     <header>
       <img src="GutenbergLogo.png" alt="Logo" className="logo" />
-      <NavLink to="/">
-        <p className="nav-chat">Chat</p>
-      </NavLink>
-      <NavLink to="/linkedin">
-        <p className="nav-linkedin">LinkedIn Connections</p>
-      </NavLink>
+      <div className="nav-items">
+        <NavLink to="/">
+          <p className="nav-chat">Chat</p>
+        </NavLink>
+        <NavLink to="/linkedin">
+          <p className="nav-linkedin">LinkedIn Connections</p>
+        </NavLink>
+      </div>
     </header>
   );
 };
