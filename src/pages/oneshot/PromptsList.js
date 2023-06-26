b8215d309579989a30eb6998e96e7b5b57c948f5
Hrishabh Jain <hrishabh507@gmail.com>
2023-06-26 16:52:01 +0530

    add prompt examples

--- src/pages/oneshot/PromptsList.js
+++ src/pages/oneshot/PromptsList.js
@@ -3,48 +3,58 @@ import styles from "./OneShot.module.css";
 
 const EXAMPLES = [
   {
-    text: "List down all the press releases for Mphasis between April 2022 and Dec 2022 in points.",
-    type: "PR",
-  },
-  // {
-  //   text: "List all the topics of bylines published for Mpahsis",
-  //   type: "PR",
-  // },
-  {
-    text: "List down the topics of bylines written for Mphasis and their key themes.",
+    text: "Can you list down and elaborate on tasks given in specific scope of services?",
     type: "PR",
   },
   {
-    text: "List all the keywords of press releases published in a time period between April 2022 and December 2022 in points.",
+    text: "Can you provide a detailed summary of the RFP for marketing for NYCEDC properties with proper headings and subheadings?",
     type: "PR",
   },
   {
-    text: "How many bylines were released under Forbes for Mphasis?",
+    text: "What is the deadline for proposal submission?",
     type: "PR",
   },
   {
-    text: "How many press releases were released under Cision for Mphasis?",
+    text: "What is the deadline for asking clarification questions?",
     type: "PR",
   },
   {
-    text: "What were the key themes, topics, industries of the releases between April 2022 and December 2022?",
+    text: "What is the process for asking these questions? Is it via an email or website/procurement portal?",
     type: "PR",
   },
   {
-    text: "How does Gutenberg value their customers?",
+    text: "Is there a pre-proposal or pre-bid meeting? Is it being held in-person / on-site? What date and time? Is a meeting link available?",
+    type: "PR",
+  },
+  {
+    text: "List the services / deliverables / scope of work.",
+    type: "PR",
+  },
+  {
+    text: "Does the RFP mention any availability of budgets or limits on budgets?",
     type: "About Us",
   },
   {
-    text: "You are the best SEO optimized content writer with an experience of 20 years. You have been tasked to write an interesting and engaging press release on the topic of 'The Growing Technology in Metaverse and Blockchain Sector in 2023' for the company Trail and tone should be very professional. Use proper headers and subheadings. The press release that you write should be with a same structure and tone as of previous Mphasis press releases but don't use the direct context from the Mphasis press releases.",
+    text: "What are the qualification criteria for agencies bidding, applying or responding to this RFP?",
     type: "Content",
   },
   {
-    text: "You are the best SEO optimized case study writer with an experience of 20 years. You have been tasked to write an interesting and engaging case study for company Trail which is a Fintech company and the tone should be very professional. Use proper headers and subheadings. The case study that you write should be with a same structure and tone as of previous Gutenberg case studies but don't use the direct context from the Gutenberg case studies.",
+    text: "Is there an Earnest Money Deposit or RFP Processing Fee required?",
+    type: "Content",
+  },
+  {
+    text: "Is there a format for technical or financial proposal that the bidders need to follow?",
+    type: "Content",
+  },
+  {
+    text: "What are the objectives or goals or aims of this RFP?",
+    type: "Content",
+  },
+  {
+    text: "Is there a list of supporting documents that the agency should submit along with the technical or financial proposal?",
     type: "Content",
   },
 ];
-
-const TYPES = ["Type", "PR", "Content", "About Us"];
 
 const PromptsList = () => {
   const [search, setSearch] = useState("");
@@ -61,17 +71,6 @@ const PromptsList = () => {
     }
   }, [search, filtered]);
 
-  const handleFilter = (e) => {
-    const value = e.target.value;
-    let filteredPrompts;
-    if (value === "type") filteredPrompts = EXAMPLES;
-    else
-      filteredPrompts = EXAMPLES.filter(
-        (prompt) => prompt.type.toLowerCase() === value
-      );
-    setFiltered([...filteredPrompts]);
-  };
-
   const handleSearch = (e) => {
     setSearch(e.target.value);
   };
@@ -85,18 +84,6 @@ const PromptsList = () => {
           className={styles.searchbar}
           placeholder="Search prompts..."
         />
-        <select
-          onChange={handleFilter}
-          className={styles.filter}
-          name="type"
-          id="type"
-        >
-          {TYPES.map((type, i) => (
-            <option key={i} value={type.toLowerCase()}>
-              {type}
-            </option>
-          ))}
-        </select>
       </div>
       {searched.map((example, i) => (
         <div className={styles.promptContainer}>
