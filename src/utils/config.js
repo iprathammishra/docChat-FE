d777d108f583b66229ced08259bf3be7f125d3f7
Hrishabh Jain <hrishabh507@gmail.com>
2023-07-24 21:16:10 +0530

    chore: change prod url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 // Constants.js
-const productionUrl = "https://docchat-be-production.up.railway.app";
+const productionUrl =
+  process.env.PRODUCTION_URL || "https://rfpreader.azurewebsites.net";
 const developmentUrl = "http://localhost:9000";
 export const BASE_URL =
   process.env.NODE_ENV === "development" ? developmentUrl : productionUrl;
